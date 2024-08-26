486c14e0319997a126da9f0da898e14ec0072f8f
MGnanda <141144302+reponanda@users.noreply.github.com>
2024-08-26 11:38:51 +0700

    update: app.js update_1

--- belajar-js/app.js
+++ belajar-js/app.js
@@ -10,30 +10,63 @@
 //     console.log (`${role} bukanlah admin!`);
 // }
 
-let day = Number (prompt ('Masukkan angka 1 sampai 7 :'));
 
-switch (day) {
-    case 1:
-        console.log ('Monday');
-        break;
-    case 2:
-        console.log ('Tuesday');
-        break;
-    case 3:
-        console.log ('Wednesday');
-        break;
-    case 4:
-        console.log ('Thursday');
-        break;
-    case 5:
-        console.log ('Friday');
-        break;
-    case 6:
-        console.log ('Saturday');
-        break;
-    case 7:
-        console.log ('Sunday');
-        break;
-    default:
-        console.log ('Input yang anda masukkan salah')
-}
+// switch pengganti if
+// let day = Number (prompt ('Masukkan angka 1 sampai 7 :'));
+
+// switch (day) {
+//     case 1:
+//         console.log ('Monday');
+//         break;
+//     case 2:
+//         console.log ('Tuesday');
+//         break;
+//     case 3:
+//         console.log ('Wednesday');
+//         break;
+//     case 4:
+//         console.log ('Thursday');
+//         break;
+//     case 5:
+//         console.log ('Friday');
+//         break;
+//     case 6:
+//         console.log ('Saturday');
+//         break;
+//     case 7:
+//         console.log ('Sunday');
+//         break;
+//     default:
+//         console.log ('Input yang anda masukkan salah')
+// }
+
+// // lexical scope
+// // function di dalam function
+// function lamarKerja() { // ketika ingin mendapatkan output maka function orangDalam() juga harus dipanggil
+//     const jabatan = 'programmer';
+
+//     function orangDalam() { // fungsi ini hanya bisa dipanggil di dalam scope lamarKerja() saja
+//         let kenalan = `orang dalam bisa memasukkan ${jabatan}`;
+//         console.log (kenalan); 
+//     }
+
+//     orangDalam(); 
+// }
+
+// // function expressions
+// const perpangkatan = function (nilai) {
+//     return nilai*nilai;
+// }
+
+// perpangkatan(5);
+
+// // function sebagai argumen dari function lain
+// function duaKali(func) {
+//     func();
+//     func();
+// }
+
+// function lemparDadu() {
+//     const hasil = Math.floor(Math.random() * 6) + 1;
+//     console.log(hasil);
+// }
